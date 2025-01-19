48a6006c23233ba4b3752b37b66b38f489b53c6e
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2025-01-19 18:07:54 +0000

    wip

--- model_diffing/utils.py
+++ model_diffing/utils.py
@@ -4,6 +4,7 @@ from typing import TypeVar
 
 import torch
 import yaml
+from einops import reduce
 from pydantic import BaseModel
 from torch import nn
 
@@ -62,3 +63,38 @@ def l2_norm(
     dtype: torch.dtype | None = None,
 ) -> torch.Tensor:
     return torch.norm(input, p=2, dim=dim, keepdim=keepdim, out=out, dtype=dtype)
+
+
+def sparsity_loss(W_dec_HMLD: torch.Tensor, hidden_BH: torch.Tensor) -> torch.Tensor:
+    assert (hidden_BH >= 0).all()
+    # each latent has a separate norms for each (model, layer)
+    W_dec_l2_norms_HML = reduce(W_dec_HMLD, "hidden model layer dim -> hidden model layer", l2_norm)
+    # to get the weighting factor for each latent, we sum it's decoder norms for each (model, layer)
+    summed_norms_H = reduce(W_dec_l2_norms_HML, "hidden model layer -> hidden", torch.sum)
+    # now we weight the latents by the sum of their norms
+    weighted_hidden_BH = hidden_BH * summed_norms_H
+    summed_weighted_hidden_B = reduce(weighted_hidden_BH, "batch hidden -> batch", torch.sum)
+    return summed_weighted_hidden_B.mean()
+
+
+def reconstruction_loss(activation_BMLD: torch.Tensor, target_BMLD: torch.Tensor) -> torch.Tensor:
+    """This is a little weird because we have both model and layer dimensions, so it's worth explaining deeply:
+
+    The reconstruction loss is a sum of squared L2 norms of the error for each activation space being reconstructed.
+    In the Anthropic crosscoders update, they don't write for the multiple-model case, so they write it as:
+
+    $$ \\sum_{l \\in L} \\|a^l(x_j) - a^{l'}(x_j)\\|^2 $$
+
+    Here, I'm assuming we want to expand that sum to be over models, so we would have:
+
+    $$ \\sum_{m \\in M} \\sum_{l \\in L} \\|a_m^l(x_j) - a_m^{l'}(x_j)\\|^2 $$
+    """
+    error_BMLD = activation_BMLD - target_BMLD
+    error_norm_BML = reduce(error_BMLD, "batch model layer d_model -> batch model layer", l2_norm)
+    squared_error_norm_BML = error_norm_BML.square()
+    summed_squared_error_norm_B = reduce(squared_error_norm_BML, "batch model layer -> batch", torch.sum)
+    return summed_squared_error_norm_B.mean()
+
+
+def get_device() -> torch.device:
+    return torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
