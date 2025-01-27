0f29f2d210d058375f923dfa04c839918922c018
Thomas Read <thjread@gmail.com>
2025-01-27 13:52:34 +0000

    Start trying out uploading weights to wandb

--- model_diffing/scripts/upload_model_to_wandb/main.py
+++ model_diffing/scripts/upload_model_to_wandb/main.py
@@ -10,19 +10,18 @@ from model_diffing.log import logger
 #     --model_checkpoint .checkpoints/path/to/checkpoint.pt
 #     --previous_run_id flimsy-cow-385 # or whatever the run id is
 
+WANDB_PROJECT = "sleeper-model-diffing"
+WANDB_ENTITY = "dmitry2-uiuc"
 
 def main(model_checkpoint: str, previous_run_id: str) -> None:
-    wandb.init(project="model-diffing", entity="mars-model-diffing")
     logger.info("Loading model checkpoint...")
     model_checkpoint_path = Path(model_checkpoint)
     assert model_checkpoint_path.exists(), f"Model checkpoint {model_checkpoint_path} does not exist."
     logger.info("Loaded model checkpoint")
-    previous_run = wandb.Api().run(f"mars-model-diffing/model-diffing/{previous_run_id}")
-    artifact = wandb.Artifact(name="model-checkpoint-v1", type="model")
-    artifact.add_file("path/to/local/checkpoint.pt")
-    wandb.log_artifact(artifact)
-    previous_run.log_artifact(artifact)
-
+    with wandb.init(entity=WANDB_ENTITY, project=WANDB_PROJECT, id=previous_run_id, resume="allow") as previous_run:
+        artifact = wandb.Artifact(name="model-checkpoint-v1", type="model")
+        artifact.add_file(model_checkpoint_path) # TODO also upload the config.yaml file
+        previous_run.log_artifact(artifact)
 
 if __name__ == "__main__":
     logger.info("Starting...")
--- model_diffing/utils.py
+++ model_diffing/utils.py
@@ -34,10 +34,9 @@ def save_model_and_config(config: BaseModel, save_dir: Path, model: nn.Module, e
         epoch: The current epoch (used in the model filename).
     """
     save_dir.mkdir(parents=True, exist_ok=True)
-    if not (save_dir / "config.yaml").exists(): # TODO should we update this if it's changed?
-        with open(save_dir / "config.yaml", "w") as f:
-            yaml.dump(config, f)
-        logger.info("Saved config to %s", save_dir / "config.yaml")
+    with open(save_dir / "config.yaml", "w") as f:
+        yaml.dump(config, f)
+    logger.info("Saved config to %s", save_dir / "config.yaml")
 
     model_file = save_dir / f"model_epoch_{epoch + 1}.pt"
     torch.save(model.state_dict(), model_file)
