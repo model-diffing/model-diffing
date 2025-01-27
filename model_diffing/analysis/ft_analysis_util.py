ec9d49fe6c8d3db161d52d86b4ebe92b82cadde0
Thomas Read <thjread@gmail.com>
2025-01-27 10:32:43 +0000

    Fix graphing script

--- model_diffing/analysis/ft_analysis_util.py
+++ model_diffing/analysis/ft_analysis_util.py
@@ -4,27 +4,24 @@ import pandas as pd
 import numpy as np
 from typing import List, Tuple, Union, Any
 from pathlib import Path
-from dataclasses import dataclass
-from torch.nn.functional import cosine_similarity
-from einops import einsum, rearrange
 
-from model_diffing.utils import create_hooked_model
-from model_diffing.models.crosscoder import AcausalCrosscoder
+from model_diffing.models.crosscoder import AcausalCrosscoder, build_topk_crosscoder
 
 
-def load_crosscoder(path: str, hidden_activation: nn.Module, device: torch.device) -> AcausalCrosscoder:
+def load_crosscoder(path: str, device: torch.device) -> AcausalCrosscoder:
     # load crosscoder model
     state_dict = torch.load(path, map_location=device)
-    crosscoder = AcausalCrosscoder(
+    crosscoder = build_topk_crosscoder( # Update this to use yaml
         n_layers=4,
         d_model=768,
-        hidden_dim=2*768,
+        cc_hidden_dim=2*768,
         dec_init_norm=0.1,
         n_models=1,
-        hidden_activation=hidden_activation,
+        k=20,
     ).to(device)
     crosscoder.load_state_dict(state_dict)
     return crosscoder
+
 
 def compute_cosine_similarities(features_1: torch.Tensor, features_2: torch.Tensor) -> np.ndarray[Any, np.dtype[np.float64]]:
     cos = nn.CosineSimilarity(dim=0, eps=1e-6)
