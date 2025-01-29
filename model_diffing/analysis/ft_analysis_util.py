Save and load normalisation scaling factors, fix and improve feature analysis

model_diffing/analysis/ft_analysis_util.py
@@ -4,23 +4,27 @@ import pandas as pd
 import numpy as np
 from typing import List, Tuple, Union, Any
 from pathlib import Path
+import yaml
 
 from model_diffing.models.crosscoder import AcausalCrosscoder, build_topk_crosscoder
+from model_diffing.scripts.train_topk_sleeper.config import TrainConfig
 
 
-def load_crosscoder(path: str, device: torch.device) -> AcausalCrosscoder:
+def load_crosscoder(path: str, epoch: int, device: torch.device) -> Tuple[AcausalCrosscoder, TrainConfig]:
     # load crosscoder model
-    state_dict = torch.load(path, map_location=device)
+    print(path)
+    state_dict = torch.load(Path(path) / f"model_epoch_{epoch}.pt", map_location=device)
+    cfg = TrainConfig(**yaml.safe_load(open(Path(path) / "config.yaml")))
     crosscoder = build_topk_crosscoder( # Update this to use yaml
         n_layers=4,
         d_model=768,
-        cc_hidden_dim=2*768,
+        cc_hidden_dim=1536,
         dec_init_norm=0.1,
         n_models=1,
         k=20,
     ).to(device)
     crosscoder.load_state_dict(state_dict)
-    return crosscoder
+    return crosscoder, cfg
 
 
 def compute_cosine_similarities(features_1: torch.Tensor, features_2: torch.Tensor) -> np.ndarray[Any, np.dtype[np.float64]]:

model_diffing/scripts/train_topk_sleeper/run.py
@@ -8,7 +8,7 @@ from model_diffing.dataloader.data import build_dataloader_BMLD, dataset_total_s
 from model_diffing.log import logger
 from model_diffing.models.crosscoder import build_topk_crosscoder
 from model_diffing.scripts.llms import build_llm_lora
-from model_diffing.scripts.train_topk_sleeper.config import TopKExperimentConfig
+from model_diffing.scripts.train_topk_sleeper.config import TopKExperimentConfig, TrainConfig
 from model_diffing.scripts.train_topk_sleeper.trainer import TopKTrainer
 from model_diffing.utils import build_wandb_run, get_device
 
@@ -42,11 +42,13 @@ def build_trainer(cfg: TopKExperimentConfig) -> TopKTrainer:
     crosscoder = crosscoder.to(device)
 
     # Load checkpoint if provided
-    if cfg.crosscoder.ft_init_checkpt is not None:
-        checkpoint_path = cfg.crosscoder.ft_init_checkpt
+    if cfg.crosscoder.ft_init_checkpt_folder is not None:
+        checkpoint_path = cfg.crosscoder.ft_init_checkpt_folder / f"model_epoch_{cfg.crosscoder.ft_init_checkpt_epoch}.pt"
         print(f"Loading checkpoint from {checkpoint_path}")
         state_dict = torch.load(checkpoint_path)
         crosscoder.load_state_dict(state_dict)
+        old_cfg = TrainConfig(**yaml.safe_load(open(cfg.crosscoder.ft_init_checkpt_folder / "config.yaml")))
+        cfg.train.norm_scaling_factors = old_cfg.norm_scaling_factors
 
     wandb_run = build_wandb_run(cfg.wandb, cfg) if cfg.wandb != "disabled" else None
 

model_diffing/scripts/train_topk_sleeper/trainer.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable, Iterator
+from pathlib import Path
 
 from itertools import islice
 import numpy as np
@@ -48,12 +49,12 @@ class TopKTrainer:
 
         self.lr_scheduler = build_lr_scheduler(cfg.optimizer, self.total_steps)
 
-    def _run_validation(self, norm_scaling_factors_ML: torch.Tensor):
+    def _run_validation(self):
         test_logs = []
         for batch_BMLD in islice(self.validation_dataloader_builder(), self.cfg.num_test_batches):
             batch_BMLD = batch_BMLD.to(self.device)
             batch_BMLD = einsum(
-                batch_BMLD, norm_scaling_factors_ML,
+                batch_BMLD, torch.tensor(self.cfg.norm_scaling_factors, device=self.device),
                 "batch model layer d_model, model layer -> batch model layer d_model"
             )
             test_logs.append(self._test_step(batch_BMLD))
@@ -63,11 +64,11 @@ class TopKTrainer:
             self.wandb_run.log(test_log, step=self.step)
         logger.info(test_log)
 
-    def _run_epoch(self, epoch: int, norm_scaling_factors_ML: torch.Tensor):
+    def _run_epoch(self, epoch: int):
         for batch_BMLD in self.dataloader_builder():
             batch_BMLD = batch_BMLD.to(self.device)
             batch_BMLD = einsum(
-                batch_BMLD, norm_scaling_factors_ML,
+                batch_BMLD, torch.tensor(self.cfg.norm_scaling_factors, device=self.device),
                 "batch model layer d_model, model layer -> batch model layer d_model"
             )
 
@@ -79,15 +80,17 @@ class TopKTrainer:
             self.step += 1
 
     def train(self):
-        logger.info("Estimating norm scaling factors (model, layer)")
+        if self.cfg.norm_scaling_factors is None:
+            logger.info("Estimating norm scaling factors (model, layer)")
+            norm_scaling_factors_ML = estimate_norm_scaling_factor_ML(
+                self.dataloader_builder(),
+                self.device,
+                self.cfg.n_batches_for_norm_estimate,
+            )
+            self.cfg.norm_scaling_factors = norm_scaling_factors_ML.tolist()
 
-        norm_scaling_factors_ML = estimate_norm_scaling_factor_ML(
-            self.dataloader_builder(),
-            self.device,
-            self.cfg.n_batches_for_norm_estimate,
-        )
+        logger.info(f"Norm scaling factors (model, layer): {self.cfg.norm_scaling_factors}")
 
-        logger.info(f"Norm scaling factors (model, layer): {norm_scaling_factors_ML}")
 
         if self.wandb_run:
             wandb.init(
@@ -97,10 +100,10 @@ class TopKTrainer:
             )
 
         # Run initial validation
-        self._run_validation(norm_scaling_factors_ML)
+        self._run_validation()
 
         for epoch in range(self.cfg.num_epochs):
-            self._run_epoch(epoch, norm_scaling_factors_ML)
+            self._run_epoch(epoch)
         
             if self.cfg.save_dir and self.cfg.save_every_n_epochs and (epoch + 1) % self.cfg.save_every_n_epochs == 0:
                 save_model_and_config(
@@ -110,7 +113,7 @@ class TopKTrainer:
                     epoch=epoch,
                 )
 
-            self._run_validation(norm_scaling_factors_ML)
+            self._run_validation()
 
     def _get_loss(self, batch_BMLD: torch.Tensor) -> tuple[torch.Tensor, np.ndarray[Any, np.dtype[np.float64]]]:
         train_res = self.crosscoder.forward_train(batch_BMLD)

model_diffing/utils.py
@@ -25,7 +25,7 @@ def build_wandb_run(wandb_config: WandbConfig, config: BaseModel) -> Run | None:
 
 
 def save_model_and_config(config: BaseModel, save_dir: Path, model: nn.Module, epoch: int) -> None:
-    """Save the model to disk. Also save the config file if it doesn't exist.
+    """Save the model to disk. Also save the config file.
 
     Args:
         config: The config object. Saved if save_dir / "config.yaml" doesn't already exist.
@@ -35,7 +35,7 @@ def save_model_and_config(config: BaseModel, save_dir: Path, model: nn.Module, e
     """
     save_dir.mkdir(parents=True, exist_ok=True)
     with open(save_dir / "config.yaml", "w") as f:
-        yaml.dump(config, f)
+        yaml.dump(config.model_dump(), f)
     logger.info("Saved config to %s", save_dir / "config.yaml")
 
     model_file = save_dir / f"model_epoch_{epoch + 1}.pt"