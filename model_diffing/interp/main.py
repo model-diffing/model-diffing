5db01c7e941f1a577db0cd4fdfc293b573649571
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2025-02-19 16:50:10 +1300

    wip

--- model_diffing/interp/main.py
+++ model_diffing/interp/main.py
@@ -1,11 +1,24 @@
+from collections.abc import Iterator
+from dataclasses import dataclass
 from itertools import islice
 from typing import Any
 
+import tabulate
 import torch
-from einops import pack, rearrange, unpack
+from einops import rearrange
+from rich.table import Table
+from rich import print as rprint
+from transformer_lens import HookedTransformer
+from transformers import PreTrainedTokenizerBase
 
 from model_diffing.data.model_hookpoint_dataloader import ScaledModelHookpointActivationsDataloader
 from model_diffing.models.crosscoder import AcausalCrosscoder
+
+
+@dataclass
+class LatentExample:
+    tokens_context_S: torch.Tensor
+    latents_context_S: torch.Tensor
 
 
 @torch.no_grad()
@@ -14,48 +27,108 @@ def gather_max_activations(
     cc: AcausalCrosscoder[Any],
     total_batches: int = 200,
     context_size: int = 20,
-):
-    n_latents = cc.hidden_dim
-    latent_examples = [[] for _ in range(n_latents)]
-    # activations_with_text.seq.special_tokens_mask_BS
+    topk_per_latent: int = 50,
+) -> list[list[LatentExample]]:
+    """
+    iterate over the dataloader, gathering context for all non-zero latents, in terms of tokens and latent activations
+
+    returns:
+        list of lists of LatentExample, where the outer list is over latents, and the inner list is over activating examples
+    """
+    latent_examples: list[list[LatentExample]] = [[] for _ in range(cc.hidden_dim)]
+
     for activations_with_text in islice(dataloader.iterate_activations_with_text(), total_batches):
         B, S = activations_with_text.activations_BSMPD.shape[:2]
         activations_BsMPD = rearrange(activations_with_text.activations_BSMPD, "b s m p d -> (b s) m p d")
         res = cc.forward_train(activations_BsMPD)
         hidden_BSH = rearrange(res.hidden_BH, "(b s) h -> b s h", b=B, s=S)
-        tokens_BS = activations_with_text.seq.tokens_BS
 
-        for latent_idx in range(n_latents):
-            latent_BS = hidden_BSH[:, :, latent_idx]
-            # padding_BS = torch.ones(B, context_size, device=latent_BS.device) * -1
-            # latent_prepad_BS = torch.cat([padding_BS, latent_BS], dim=1)
+        # zero out the activations that are not in the topk
+        vals, indices = hidden_BSH.topk(topk_per_latent, dim=-1)
+        hidden_BSH = torch.zeros_like(hidden_BSH).scatter_(-1, indices, vals)
 
-            batch_indices, seq_pos_indices = latent_BS.nonzero(as_tuple=True)
-            seq_pos_start_indices = (seq_pos_indices - context_size).clamp(min=0)
+        for latent_idx in range(hidden_BSH.shape[-1]):
+            hidden_BS = hidden_BSH[:, :, latent_idx]
+            this_latent_examples = collect_examples(hidden_BS, activations_with_text.tokens_BS, context_size)
+            latent_examples[latent_idx].extend(this_latent_examples)
 
-            for firing_batch, firing_seq_pos_start, firing_seq_pos_end in zip(
-                batch_indices, seq_pos_start_indices, seq_pos_indices, strict=False
-            ):
-                tokens_context_S = tokens_BS[firing_batch, firing_seq_pos_start:firing_seq_pos_end]
-                latent_context_S = latent_BS[firing_batch, firing_seq_pos_start:firing_seq_pos_end]
+    # within each latent, sort by activation strength.
+    for latent_idx in range(cc.hidden_dim):
+        latent_examples[latent_idx].sort(key=lambda x: x.latents_context_S[-1].item())
 
-                latent_examples[latent_idx].append(
-                    {
-                        "tokens_context_S": tokens_context_S,
-                        "latents_context_S": latent_context_S,
-                    }
-                )
-
-            # latent_examples[latent_idx].extend(...)
+    return latent_examples
 
 
-# def get_latents(
-#     cc: AcausalCrosscoder[Any],
-#     activations_BSMPD: torch.Tensor,
-# ) -> torch.Tensor:
-#     return hidden_BSH
+def collect_examples(latents_BS: torch.Tensor, tokens_BS: torch.Tensor, context_size: int) -> Iterator[LatentExample]:
+    batch_indices, seq_pos_indices = latents_BS.nonzero(as_tuple=True)
+    seq_pos_start_indices = (seq_pos_indices - context_size).clamp(min=0)
+    for firing_batch, firing_seq_pos_start, firing_seq_pos_end in zip(
+        batch_indices, seq_pos_start_indices, seq_pos_indices, strict=False
+    ):
+        yield LatentExample(
+            tokens_context_S=tokens_BS[firing_batch, firing_seq_pos_start:firing_seq_pos_end],
+            latents_context_S=latents_BS[firing_batch, firing_seq_pos_start:firing_seq_pos_end],
+        )
 
-# t = torch.tensor([[0,
 
-# and the predicted token (?? actually, this is the next token, not the predicted token)
-# firing_seq_pos_end = (firing_seq_pos + 1).clamp(max=S)
+def display_latent_examples(latent_examples: list[LatentExample], tokenizer: PreTrainedTokenizerBase):
+    for i, example in enumerate(latent_examples):
+        print(f"Example {i}")
+        print(tokenizer.decode(example.tokens_context_S))
+        print(example.latents_context_S)
+        print()
+
+
+def top_and_bottom_logits(
+    llm: HookedTransformer,
+    sae_W_dec_HD: torch.Tensor,
+    k: int = 10,
+):
+    for latent_idx in range(sae_W_dec_HD.shape[0]):
+        logits = sae_W_dec_HD[latent_idx] @ llm.W_U
+        pos_logits, pos_token_ids = logits.topk(k)
+        pos_tokens = llm.to_str_tokens(pos_token_ids)
+        neg_logits, neg_token_ids = logits.topk(k, largest=False)
+        neg_tokens = llm.to_str_tokens(neg_token_ids)
+
+        print(
+            tabulate.tabulate(
+                zip(map(repr, neg_tokens), neg_logits, map(repr, pos_tokens), pos_logits, strict=True),
+                headers=["Bottom tokens", "Value", "Top tokens", "Value"],
+                tablefmt="simple_outline",
+                stralign="right",
+                numalign="left",
+                floatfmt="+.3f",
+            )
+        )
+
+
+def display_top_seqs(data: list[tuple[float, list[str]]]):
+    """
+    Given a list of (activation: float, str_toks: list[str]), displays a table of these sequences, with
+    the relevant token highlighted.
+
+    We also turn newlines into "\\n", and remove unknown tokens � (usually weird quotation marks) for readability.
+    """
+    table = Table("Act", "Sequence", title="Max Activating Examples", show_lines=True)
+    for act, str_toks in data:
+        formatted_seq = (
+            "".join(
+                [
+                    f"[b u green]{str_tok}[/]" if i == len(str_toks) - 1 else str_tok
+                    for i, str_tok in enumerate(str_toks)
+                ]
+            )
+            .replace("�", "")
+            .replace("\n", "↵")
+        )
+        table.add_row(f"{act:.3f}", repr(formatted_seq))
+    rprint(table)
+
+    # DEMO_data = [
+    #     (1.0, ["Hello", " world", "!"]),
+    #     (0.5, ["Hello", " there", "!"]),
+    #     (0.0, ["Hello", "!", " there"]),
+    # ]
+
+    # display_top_seqs(DEMO_data)
